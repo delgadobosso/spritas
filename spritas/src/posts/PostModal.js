d1893d659e2de95dce92222dba2d83c3d3cbe708
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-01-14 21:09:08 -0500

    Using aspect ratio to present images

--- spritas/src/posts/PostModal.js
+++ spritas/src/posts/PostModal.js
@@ -4,12 +4,15 @@ import React from 'react';
 export default class PostModal extends React.Component {
     constructor(props) {
         super(props);
+        this.handleResize = this.handleResize.bind(this);
         this.toggleRender = this.toggleRender.bind(this);
-        this.toggleFill = this.toggleFill.bind(this);
+        this.toggleZoom = this.toggleZoom.bind(this);
         this.state = ({
             image: null,
+            container: null,
+            resize: true,
             imgSharp: true,
-            imgFill: false
+            imgZoom: false
         });
     }
 
@@ -18,8 +21,28 @@ export default class PostModal extends React.Component {
         setTimeout(() => modal.style.opacity = 1, 10);
 
         const image = new Image();
-        image.onload = (e) => this.setState({ image: e.target });
+        image.onload = (e) => {
+            const container = document.getElementById(`PostModal-imageContainer-${this.props.id}`);
+            this.setState({
+                image: image,
+                container: container
+            });
+        }
         image.src = this.props.link;
+
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
+
+    handleResize() {
+        // Throttle resize handle
+        if (this.state.resize) {
+            this.setState({ resize: false });
+            setTimeout(() => this.setState({ resize: true }), 500);
+        }
     }
 
     toggleRender() {
@@ -28,13 +51,28 @@ export default class PostModal extends React.Component {
         }));
     }
 
-    toggleFill() {
+    toggleZoom() {
         this.setState(state => ({
-            imgFill: !state.imgFill
+            imgZoom: !state.imgZoom
         }));
     }
 
     render() {
+        var resWidth = 'initial';
+        var resHeight = 'initial';
+        if (this.state.image && this.state.container) {
+            const image = this.state.image;
+            const container = this.state.container;
+            var ratio;
+            if (image.width < (container.clientWidth * 0.5) && image.height < (container.clientHeight * 0.5)) {
+                ratio = Math.min((container.clientWidth * 0.5) / image.width, (container.clientHeight * 0.5) / image.height);
+            } else {
+                ratio = Math.min(container.clientWidth / image.width, container.clientHeight / image.height);
+            }
+            resWidth = image.width * ratio;
+            resHeight =  image.height *  ratio;
+        }
+
         var imgRend;
         var rendLabel;
         if (this.state.imgSharp) {
@@ -44,27 +82,26 @@ export default class PostModal extends React.Component {
             imgRend = 'auto';
             rendLabel = 'Smooth';
         }
+
         const imgStyle = {
+            width: resWidth,
+            height: resHeight,
             imageRendering: imgRend
         };
-
-        if (this.state.imgFill) {
-            console.log('lol');
-        }
 
         return (
             <div className='PostModal' id={'PostModal-' + this.props.id}>
                 <div className='PostModal-backing'></div>
-                <div className='PostModal-container'>
+                <div className={'PostModal-container'}>
                     <div className='PostModal-imageControls'>
                         <div className='PostModal-imageButton'
                             onClick={() => window.history.go(-1)}>Close Image</div>
                     </div>
-                    <div className='PostModal-imageContainer'>
-                        <img className='PostModal-img'
+                    <div id={`PostModal-imageContainer-${this.props.id}`} className={`PostModal-imageContainer`}>
+                        <img id={`PostModal-img-${this.props.id}`} className={`PostModal-img`}
                             src={this.props.link}
                             alt="Modal"
-                            onClick={this.toggleFill}
+                            onClick={this.toggleZoom}
                             style={imgStyle} />
                     </div>
                     <div className='PostModal-imageControls'>
