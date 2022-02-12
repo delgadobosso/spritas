1dbe27b47951dc1395fb2f339ccf3876f3e5a3c8
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-02-12 00:46:04 -0500

    working on better zoom and drag for modal

--- spritas/src/posts/PostModal.js
+++ spritas/src/posts/PostModal.js
@@ -5,15 +5,19 @@ export default class PostModal extends React.Component {
     constructor(props) {
         super(props);
         this.handleResize = this.handleResize.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleMove = this.handleMove.bind(this);
         this.toggleRender = this.toggleRender.bind(this);
         this.toggleZoom = this.toggleZoom.bind(this);
         this.state = ({
             image: null,
+            imgElem: null,
             container: null,
             resize: true,
             imgSharp: true,
             imgZoom: false,
-            everZoomed: false
+            everZoomed: false,
+            dragging: false
         });
     }
 
@@ -26,15 +30,29 @@ export default class PostModal extends React.Component {
             const container = document.getElementById(`PostModal-imageContainer-${this.props.id}`);
             this.setState({
                 image: image,
+                imgElem: imgElem,
                 container: container
             });
         }
+        const imgElem = document.getElementById(`PostModal-img-${this.props.id}`);
         image.src = this.props.link;
 
+        imgElem.addEventListener('mousedown', this.handleClick);
+        imgElem.addEventListener('touchstart', this.handleClick);
+        imgElem.addEventListener('mouseup', this.handleClick);
+        imgElem.addEventListener('touchend', this.handleClick);
+        window.addEventListener('mousemove', this.handleMove);
+        window.addEventListener('touchmove', this.handleMove);
         window.addEventListener('resize', this.handleResize);
     }
 
     componentWillUnmount() {
+        window.removeEventListener('mousedown', this.handleClick);
+        window.removeEventListener('touchstart', this.handleClick);
+        window.removeEventListener('mouseup', this.handleClick);
+        window.removeEventListener('touchend', this.handleClick);
+        window.removeEventListener('mousemove', this.handleMove);
+        window.removeEventListener('touchmove', this.handleMove);
         window.removeEventListener('resize', this.handleResize);
     }
 
@@ -43,6 +61,25 @@ export default class PostModal extends React.Component {
         if (this.state.resize) {
             this.setState({ resize: false });
             setTimeout(() => this.setState({ resize: true }), 500);
+        }
+    }
+
+    handleClick(e) {
+        if (this.state.imgZoom) {
+            if (e.type === 'mousedown' || e.type === 'touchstart') this.setState({ dragging: true });
+            else this.setState({ dragging: false });
+        } else this.toggleZoom();
+    }
+
+    handleMove(e) {
+        if (this.state.dragging) {
+            var transform = this.state.imgElem.style.transform;
+            if (e.type === 'mousemove') {
+                this.state.imgElem.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+            } else if (e.type === 'touchmove') {
+                var touch = e.touches[0] || e.changedTouches[0];
+                this.state.imgElem.style.transform = `translate(${touch.pageX}px, ${touch.pageY}px)`;
+            }
         }
     }
 
@@ -70,6 +107,7 @@ export default class PostModal extends React.Component {
         var resHeight = 'initial';
         var offset = '';
         var trans = (this.state.everZoomed) ? 'width 0.5s, height 0.5s, transform 0.5s' : 'width 0.5s, height 0.5s';
+        var cursor = (this.state.imgZoom) ? 'move' : 'initial';
         if (this.state.image && this.state.container) {
             const image = this.state.image;
             const container = this.state.container;
@@ -98,6 +136,7 @@ export default class PostModal extends React.Component {
             width: resWidth,
             height: resHeight,
             imageRendering: imgRend,
+            cursor: cursor,
             transform: offset,
             transition: trans
         };
@@ -117,8 +156,8 @@ export default class PostModal extends React.Component {
                             className={`PostModal-img`}
                             src={this.props.link}
                             alt="Modal"
-                            onClick={this.toggleZoom}
-                            style={imgStyle} />
+                            style={imgStyle}
+                            draggable='false' />
                     </div>
                     <div className='PostModal-imageControls'>
                         <div className='PostModal-imageButton'
