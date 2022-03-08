5c74a0b51e54992157129c026015685a7cea4b95
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-03-07 23:03:38 -0500

    simple featured added

--- spritas/src/featured/Featured.js
+++ spritas/src/featured/Featured.js
@@ -4,13 +4,28 @@ import './Featured.css';
 export default class Featured extends React.Component {
     constructor(props) {
         super(props);
+        this.scrollTo = this.scrollTo.bind(this);
+    }
+
+    scrollTo() {
+        var feat = document.getElementById('featured');
+        feat.scrollIntoView({ behavior: "smooth" });
+        if (window.location.hash !== "#featured") window.history.pushState({}, "", "#featured");
     }
 
     render() {
         return (
-            <div className='Featured'>
-                <div className='Featured-header'>
+            <div id='featured' className='Featured'>
+                <div className='Featured-header' onClick={this.scrollTo}>
                     <h1 className='Featured-title'>Featured</h1>
+                </div>
+                <div className='Featured-videoContainer'>
+                    <div className='Featured-video'>
+                        <iframe width='100%' height='675'
+                            title='Featured-Video' allowFullScreen
+                            src='https://www.youtube.com/embed/jIGyeOXJpwA?modestbranding=1'>
+                        </iframe>
+                    </div>
                 </div>
             </div>
         );
