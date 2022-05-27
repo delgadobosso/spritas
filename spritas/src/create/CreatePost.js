4b578d69ea492be885e358222a8d070d18770968
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-05-26 21:44:56 -0400

    frontend video upload preview

--- spritas/src/create/CreatePost.js
+++ spritas/src/create/CreatePost.js
@@ -6,23 +6,34 @@ export default class CreatePost extends React.Component {
     constructor(props) {
         super(props);
         this.handleImg = this.handleImg.bind(this);
-        this.state = { imgPreview: null };
+        this.canvasRef = React.createRef();
+        this.videoRef = React.createRef();
+        this.state = {
+            imgPreview: null,
+            videoUp: false
+        };
     }
 
     handleImg(e) {
         const file = e.target.files[0];
 
         // Check file size
-        if (file.size > 1048576) {
+        if (file.size > 20971520) {
             e.target.value = '';
-            alert('The file you selected is too large. Image must be 1 MB or less.');
+            alert('The file you selected is too large. Image must be 20 MB or less.');
         } else {
-            const reader = new FileReader();
-    
-            if (!file.type.startsWith('image/')) return;
-        
-            reader.onload = ((e) => { this.setState({ imgPreview: e.target.result }); });
-            reader.readAsDataURL(file);
+            if (file.type.startsWith('image/')) {
+                const reader = new FileReader();
+                reader.onload = ((e) => { this.setState({ imgPreview: e.target.result }); });
+                reader.readAsDataURL(file);
+            } else if (file.type.startsWith('video/')) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    this.videoRef.current.src = e.target.result;
+                    this.videoRef.current.load()
+                }
+                reader.readAsDataURL(file);
+            }
         }
     }
 
@@ -34,8 +45,18 @@ export default class CreatePost extends React.Component {
         const link = (type === "VIDO") ?
         <div className="CreatePost-item">
             <label htmlFor="link">Link: </label>
-            <input type="text" name="link" id="link" required pattern={regex_video} />
-        </div> : <input type="hidden" name="link" id="link" value="null" />;
+            <input type="text" name="link" id="link" pattern={regex_video} />
+        </div>
+        : <input type="hidden" name="link" id="link" value="null" />;
+
+        const videoFile = (type === "VIDO") ?
+        <div className="CreatePost-item">
+            <label htmlFor="videoFile">Video File: </label>
+            <input type="file" name="videoFile" id="videoFile"
+                onChange={this.handleImg}
+                accept="video/mp4, video/webm" />
+        </div>
+        : <input type="hidden" name="videoFile" id="videoFile" value="null" />;
 
         const file = (type === "IMG") ?
         <div className="CreatePost-item">
@@ -44,10 +65,10 @@ export default class CreatePost extends React.Component {
                 onChange={this.handleImg}
                 accept="image/png, image/jpeg, image/gif" />
         </div> : <input type="hidden" name="file" id="file" value="null" />;
-        const imgPreview = (type === "IMG") ?
+        const imgPreview = (type === "IMG" && this.state.imgPreview) ?
         <img className="CreatePost-imgPreview" src={this.state.imgPreview} alt="Preview" /> : null;
 
-        const enctype = (type === "IMG") ? "multipart/form-data" : null;
+        const enctype = (type === "IMG" || this.state.videoUp) ? "multipart/form-data" : null;
 
         return (
             <div className="CreatePost">
@@ -64,8 +85,10 @@ export default class CreatePost extends React.Component {
                         <label htmlFor="subtitle">Subtitle: </label>
                         <input type="text" name="subtitle" id="subtitle" maxLength="30" />
                     </div>
+                    {videoFile}
                     {link}
                     {file}
+                    <video controls ref={this.videoRef} width="640" height="360" loop muted />
                     {imgPreview}
                     <div className="CreatePost-item">
                         <label htmlFor="body">Body: </label>
