fac9c95389a08a6d611ded487b0a3e961016ad30
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-07-04 18:25:19 -0400

    front end audit log fetch

--- spritas/src/admin/AuditLog.js
+++ spritas/src/admin/AuditLog.js
@@ -4,6 +4,40 @@ import './AuditLog.css';
 export default class AuditLog extends React.Component {
     constructor(props) {
         super(props);
+        this.audit = this.audit.bind(this);
+        this.state = {
+            audit: [],
+            offset: 0,
+            amount: 10,
+            more: true
+        }
+    }
+
+    componentDidMount() {
+        this.audit();
+    }
+
+    audit() {
+        fetch(`/admin/audit/${this.state.offset}.${this.state.amount}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.length > 0) {
+                var newItems = data.slice(0, this.state.amount).map((item, index) =>
+                    <tr>
+                        <td>{item.idFrom}</td>
+                        <td>{item.idTo}</td>
+                        <td>{item.idContent}</td>
+                        <td>{item.type}</td>
+                        <td>{item.reason}</td>
+                        <td>{item.ts}</td>
+                    </tr>
+                );
+                console.log(newItems);
+                if (data.length < (this.state.amount + 1)) this.setState({ more: false });
+                else this.setState(state => ({ offset: state.offset + this.state.amount }));
+                this.setState(state => ({ audit: [newItems, ...state.audit] }));
+            }
+        })
     }
 
     render() {
@@ -11,9 +45,12 @@ export default class AuditLog extends React.Component {
             <table className='AuditLog'>
                 <thead>
                     <tr>
-                        <th>Stuff</th>
+                        <th>Audit Log</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {this.state.audit}
+                </tbody>
             </table>
         )
     }
