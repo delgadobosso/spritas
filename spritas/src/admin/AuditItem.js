c9f6e5a04472a28477037444d3a12fc2ad8fe878
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-07-06 00:46:46 -0400

    better line breaking

--- spritas/src/admin/AuditItem.js
+++ spritas/src/admin/AuditItem.js
@@ -27,11 +27,11 @@ export default class AuditItem extends React.Component {
             case 'RP':
                 result = (
                     <span>
-                        <a href={`/u/${item.usernameFrom}`}>{`${item.nicknameFrom} (@${item.usernameFrom})`}</a>
-                        &nbsp;reported post&nbsp;
-                        <a href={`/post/${item.idContent}`} onClick={this.handleClick}>{`#${item.idContent}`}</a>
-                        &nbsp;by&nbsp;
-                        <a href={`/u/${item.usernameTo}`}>{`${item.nicknameTo} (@${item.usernameTo})`}</a>
+                        <a href={`/u/${item.usernameFrom}`}>{`${item.nicknameFrom} (@${item.usernameFrom})`}</a>&nbsp;
+                        <span>reported post&nbsp;
+                        <a href={`/post/${item.idContent}`} onClick={this.handleClick}>{`#${item.idContent}`}</a>&nbsp;</span>
+                        <span>by&nbsp;
+                        <a href={`/u/${item.usernameTo}`}>{`${item.nicknameTo} (@${item.usernameTo})`}</a></span>
                     </span>
                 );
                 break;
