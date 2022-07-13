72dbccfac9a8a47546f7db76a04621441b485226
Bryan Delgado Bosso <bryan.delgado.bosso@gmail.com>
2022-07-13 00:40:50 -0400

    login/register styling consistency

--- spritas/src/login/Login.js
+++ spritas/src/login/Login.js
@@ -6,51 +6,53 @@ export default function Login() {
     return(
         <div className="Login-forms">
             <form action="/login/signin" className="Login-form" method="POST">
-                <h1>Login</h1>
+                <h1 className="Login-title">Login</h1>
                 <div className="Login-item">
-                    <label htmlFor="username">Username: </label>
-                    <input type="text" name="username" id="username" required></input>
+                    <label className="sr-only" htmlFor="username">Username</label>
+                    <span className="Login-at">@ </span>
+                    <input type="text" name="username" id="username" required placeholder="username"></input>
                 </div>
                 <div className="Login-item">
-                    <label htmlFor="pass">Password: </label>
-                    <input type="password" name="pass" id="pass" required></input>
+                    <label className="sr-only" htmlFor="pass">Password</label>
+                    <input type="password" name="pass" id="pass" required placeholder="Password"></input>
                 </div>
                 <div className="Login-item">
-                    <input type="submit" value="Login"></input>
+                    <input className="Login-submit" type="submit" value="Login"></input>
                 </div>
             </form>
 
             <form action="/login/signup" className="Login-form" method="POST">
-                <h1>Register</h1>
+                <h1 className="Login-title">Register</h1>
                 <div className="Login-item">
-                    <label htmlFor="username">Username: </label>
+                    <label className="sr-only" htmlFor="username">Username</label>
+                    <span className="Login-at">@ </span>
                     <input type="text" name="username" id="username"
-                    minlength="2" required></input>
+                    minlength="2" required placeholder="username"></input>
                 </div>
                 <div className="Login-item">
-                    <label htmlFor="nickname">Display Name: </label>
+                    <label className="sr-only" htmlFor="nickname">Display Name</label>
                     <input type="text" name="nickname" id="nickname"
-                    minlength="2" required></input>
+                    minlength="2" required placeholder="Display Name"></input>
                 </div>
                 <div className="Login-item">
-                    <label htmlFor="pass">Password: </label>
+                    <label className="sr-only" htmlFor="pass">Password</label>
                     <input type="password" name="pass" id="pass"
-                    minlength="8" required></input>
+                    minlength="8" required placeholder="Password"></input>
                 </div>
                 <div className="Login-item">
-                    <label htmlFor="pass-confirm">Confirm Your Password: </label>
-                    <input type="password" name="pass-confirm" id="pass-confirm" required></input>
+                    <label className="sr-only" htmlFor="pass-confirm">Confirm Password</label>
+                    <input type="password" name="pass-confirm" id="pass-confirm" required placeholder="Confirm Password"></input>
                 </div>
                 <div className="Login-item">
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" id="email" required></input>
+                    <label className="sr-only" htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" required placeholder="Email"></input>
                 </div>
                 <div className="Login-item">
-                    <label htmlFor="email-confirm">Confirm Your Email: </label>
-                    <input type="email" name="email-confirm" id="email-confirm" required></input>
+                    <label className="sr-only" htmlFor="email-confirm">Confirm Email</label>
+                    <input type="email" name="email-confirm" id="email-confirm" required placeholder="Confirm Email"></input>
                 </div>
                 <div className="Login-item">
-                    <input type="submit" value="Register"></input>
+                    <input className="Login-submit" type="submit" value="Register"></input>
                 </div>
             </form>
         </div>
